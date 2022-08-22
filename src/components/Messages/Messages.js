c1983c393af4cf3340b175b3b3da9026eff93c0f
Verruckt1eo <levmorozov30112005@gmail.com>
2022-08-22 19:52:37 +0300

    Started Messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,7 +1,52 @@
+import {useState} from "react"
+let chat = {
+    interlocutor:"Иван",
+    messages:[
+        {
+            senderName:"Иван",
+            messageText:"Го в кс",
+            messageTime:new Date().toLocaleDateString(),
+        },
+        {
+            senderName:"lev",
+            messageText:"Го",
+            messageTime:new Date().toLocaleDateString(),
+        }
+    ] 
+}
+let chat2 = {
+    interlocutor:"Артём",
+    messages:[
+        {
+            senderName:"Lev",
+            messageText:"Помоги с дз",
+            messageTime:new Date().toLocaleDateString(),
+        },
+        {
+            senderName:"Артём",
+            messageText:"Сор я на дискаче",
+            messageTime:new Date().toLocaleDateString(),
+        }
+    ] 
+}
 function Messages(props){
+    let [chats,setChats] = useState([chat,chat2])
     return(
         <div>
             <h1>Чаты</h1>
+            <ul>
+                {
+                    chats.map((chat)=>{
+                        return(
+                            <li>
+                                <p>{chat.interlocutor}</p>
+                                <p>{chat.messages[1].messageText}</p>
+                                <p>{chat.messages[1].messageTime}</p>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
